49e306f6d51f040b165ad76c3173e51d785ce214
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2026-03-18 12:38:48 -0300

    add mongodb

---
+++ pkg/database/mongoDB/command_test.go
@@ -0,0 +1,126 @@
+package mongoDB
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestCommandWithoutInitialize(t *testing.T) {
+
+	ctx := context.Background()
+	mongoDBInstance = nil
+
+	t.Run("Should return error when execute command", func(t *testing.T) {
+		err := NewCommand[User](ctx, nil).InsertOne()
+		assert.EqualError(t, err, dbNotInitializedError)
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		err := NewCommand[User](ctx, nil).DeleteOne()
+		assert.EqualError(t, err, dbNotInitializedError)
+	})
+}
+
+func TestInsertOne(t *testing.T) {
+	ctx := context.Background()
+	InitializeMongoDBTest(ctx)
+
+	t.Run("Should return error when execute a command", func(t *testing.T) {
+		err := NewCommand[User](ctx, nil).InsertOne()
+		assert.EqualError(t, err, modelIsEmptyError)
+	})
+
+	t.Run("Should execute insert one", func(t *testing.T) {
+		birth, _ := time.Parse("2006-01-02", "2021-11-22")
+		user := &User{"123", "Usuário teste insert one", birth, Profile{"100", "ADMIN"}}
+
+		err := NewCommand[User](ctx, user).InsertOne()
+		assert.Nil(t, err)
+
+		filter := bson.M{"name": "Usuário teste insert one"}
+		result, err := NewQuery[User](ctx, filter).One()
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, "Usuário teste insert one", result.Name)
+	})
+
+	t.Run("Should execute insert many", func(t *testing.T) {
+		birth, _ := time.Parse("2006-01-02", "2021-11-22")
+		userA := &User{"123a", "Usuário teste insert many", birth, Profile{"100", "ADMIN"}}
+		userB := &User{"123b", "Usuário teste insert many", birth, Profile{"100", "ADMIN"}}
+
+		users := []*User{userA, userB}
+
+		err := NewCommand[User](ctx, users).InsertMany()
+		assert.Nil(t, err)
+
+		filter := bson.M{"name": "Usuário teste insert many"}
+		result, err := NewQuery[User](ctx, filter).Many()
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, 2, len(result))
+		assert.Equal(t, "123a", result[0].ID)
+		assert.Equal(t, "123b", result[1].ID)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	ctx := context.Background()
+	InitializeMongoDBTest(ctx)
+
+	t.Run("Should return error when DeleteOne", func(t *testing.T) {
+		err := NewCommand[User](ctx, nil).DeleteOne()
+		assert.EqualError(t, err, modelIsEmptyError)
+	})
+
+	t.Run("Should execute delete one", func(t *testing.T) {
+		birth, _ := time.Parse("2006-01-02", "2021-11-22")
+		user := &User{"1234", "Usuário teste delete one", birth, Profile{"100", "ADMIN"}}
+
+		err := NewCommand[User](ctx, user).InsertOne()
+		assert.Nil(t, err)
+
+		filter := bson.M{"name": "Usuário teste delete one"}
+		result, err := NewQuery[User](ctx, filter).One()
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, "Usuário teste delete one", result.Name)
+
+		err = NewCommand[User](ctx, user).DeleteOne()
+		assert.Nil(t, err)
+
+		result, err = NewQuery[User](ctx, filter).One()
+		assert.NoError(t, err)
+		assert.Nil(t, result)
+	})
+
+	t.Run("Should execute delete many", func(t *testing.T) {
+		birth, _ := time.Parse("2006-01-02", "2021-11-22")
+		userA := &User{"123c", "Usuário teste delete many", birth, Profile{"100", "ADMIN"}}
+		userB := &User{"123d", "Usuário teste delete many", birth, Profile{"100", "ADMIN"}}
+
+		users := []*User{userA, userB}
+
+		err := NewCommand[User](ctx, users).InsertMany()
+		assert.Nil(t, err)
+
+		filter := bson.M{"name": "Usuário teste delete many"}
+		result, err := NewQuery[User](ctx, filter).Many()
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, 2, len(result))
+		assert.Equal(t, "123c", result[0].ID)
+		assert.Equal(t, "123d", result[1].ID)
+
+		err = NewCommand[User](ctx, users).DeleteMany()
+		assert.Nil(t, err)
+
+		result, err = NewQuery[User](ctx, filter).Many()
+		assert.NoError(t, err)
+		assert.Nil(t, result)
+	})
+}
---
+++ pkg/database/mongoDB/mongo_db_util.go
@@ -0,0 +1,169 @@
+package mongoDB
+
+import (
+	"context"
+	"reflect"
+	"regexp"
+
+	"github.com/colibriproject-dev/colibri-sdk-go/pkg/base/config"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type MongoDBModel interface {
+	CollectionName() string
+	GetID() string
+}
+
+// getDataList retrieves a list of items from the given mongo.Client object.
+//
+// It takes a mongo.Client object as input and returns a list of items of type T and an error.
+func getDataList[T any](cursor *mongo.Cursor) ([]T, error) {
+	list := make([]T, 0)
+	exist := false
+	for cursor.Next(context.Background()) {
+		exist = true
+		var model T
+		err := cursor.Decode(&model)
+		if err != nil {
+			return nil, err
+		}
+
+		list = append(list, model)
+	}
+
+	if exist {
+		return list, nil
+	}
+
+	return nil, nil
+}
+
+// getMongoCollection returns a instance of mongo.Collection
+//
+// instance of mongo.Client and a instance of interface
+// returns mongo.Collection
+func getMongoCollection(instance *mongo.Client, model MongoDBModel) *mongo.Collection {
+	collectionName := model.CollectionName()
+	return instance.Database(MONGODB_NAME).Collection(collectionName)
+}
+
+// isStructEmpty return if object is an empty structure
+//
+// any object
+// return bool if object is empty
+func isStructEmpty(s interface{}) bool {
+	val := reflect.ValueOf(s)
+
+	if isPointer(val) {
+		if val.IsNil() {
+			return true
+		}
+		val = val.Elem()
+	}
+
+	if isSliceOrArray(val) {
+		return val.Len() == 0
+	}
+
+	if isStruct(val) {
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			switch field.Kind() {
+			case reflect.String:
+				if field.String() != "" {
+					return false
+				}
+			case reflect.Map, reflect.Slice:
+				if field.Len() != 0 {
+					return false
+				}
+			case reflect.Struct:
+				if !isStructEmpty(field.Interface()) {
+					return false
+				}
+			case reflect.Ptr:
+				if !field.IsNil() && !isStructEmpty(field.Interface()) {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+// isPointer return if val is a pointer
+//
+// reflect.Value
+// return bool if val is a pointer
+func isPointer(val reflect.Value) bool {
+	return val.Kind() == reflect.Ptr
+}
+
+// isSliceOrArray return if val is a slice or an array
+//
+// reflect.Value
+// return bool if val is a slice or an array
+func isSliceOrArray(val reflect.Value) bool {
+	return val.Kind() == reflect.Slice || val.Kind() == reflect.Array
+}
+
+// isStruct return if val is a struct
+//
+// reflect.Value
+// return bool if val is a struct
+func isStruct(val reflect.Value) bool {
+	return val.Kind() == reflect.Struct
+}
+
+// buildFilterFromModels return a bson.M containig a filter with the Ids of a []interface{}
+//
+// interface{}
+// return a bson.M containig a filter with the Ids of a []interface{} or an error
+func buildFilterFromModel(models []MongoDBModel) (bson.M, error) {
+	ids := make([]interface{}, len(models))
+
+	for i, model := range models {
+		ids[i] = model.GetID()
+	}
+
+	// Create a filter to delete
+	filter := bson.M{"_id": bson.M{"$in": ids}}
+
+	return filter, nil
+}
+
+// getReadPref return a readpref.ReadPref
+//
+// return a readpref.ReadPref connection
+func getReadPref() *readpref.ReadPref {
+	if config.MONGODB_READPREF == "" {
+		return nil
+	}
+
+	switch config.MONGODB_READPREF {
+	case config.MONGODB_READPREF:
+		return readpref.Secondary()
+	case config.MONGODB_READPREF_SECONDARY:
+		return readpref.SecondaryPreferred()
+	default:
+		return readpref.Primary()
+	}
+}
+
+func extractMongoDBNameFromURI(uri string) string {
+	if uri == "" {
+		return ""
+	}
+
+	re := regexp.MustCompile(`mongodb:\/\/[^\/]+\/([^?]+)`)
+	match := re.FindStringSubmatch(uri)
+
+	if len(match) > 1 {
+		return match[1]
+	}
+
+	return ""
+}
